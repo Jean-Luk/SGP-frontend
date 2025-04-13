042f210267cf3e4a57872e4000e7f30c04670a8d
Jean-Luk <jeannfantozzi@gmail.com>
2025-04-13 19:30:39 -0300

    feat: adicionar página de vendedores

---
+++ src/components/CardVendedor.jsx
@@ -0,0 +1,17 @@
+const CardVendedor = (prop) => {
+    return (
+        <article
+            key={prop.vendedor._id}
+            className="w-full bg-white rounded p-2 relative hover:scale-102 duration-200">
+
+            <h1 className="text-2xl">{prop.vendedor?.nome}</h1>
+            <p><span className="font-medium">Código:</span> {prop.vendedor?.codVendedor}</p>
+            <p><span className="font-medium">Total de retiradas:</span> {prop.vendedor?.totalRetiradas}</p>
+            <p><span className="font-medium">Total de adições:</span> {prop.vendedor?.totalAdicoes}</p>
+
+        </article>
+
+    )
+}
+
+export default CardVendedor;
---
+++ src/pages/Vendedores.jsx
@@ -0,0 +1,96 @@
+import { api } from '../services/api.js'
+import { useEffect, useState, useRef } from "react";
+import CardVendedor from "../components/CardVendedor.jsx";
+
+export default function App() {
+
+  const [vendedores, setVendedores] = useState([]);
+  const [erro, setErro] = useState([]);
+
+  useEffect(() => {
+    loadVendedores();
+  }, [])
+
+
+  async function loadVendedores() {
+    const response = await api.get("/vendedores/listar")
+    setVendedores(response.data);
+  }
+
+  const nomeRef = useRef(null)
+  const codRef = useRef(null)
+  const pinRef = useRef(null)
+
+  const handleChange = (setter) => (event) => {
+    setter(event.target.value);
+  }
+
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    if (!nomeRef.current?.value || !codRef.current?.value || !pinRef.current?.value) return setErro("Preencha todos os campos");
+
+    try {
+      const response = await api.post("/vendedores/criar", {
+        nome: nomeRef.current?.value,
+        pin: pinRef.current?.value,
+        codVendedor: codRef.current?.value,
+      })
+
+      setErro(false);
+      setVendedores(todosVendedores => [...todosVendedores, response.data])
+    } catch (err) {
+      setErro(err.response?.data?.erro || "Ocorreu um erro. Tente novamente mais tarde")
+    }
+
+  }
+
+  return (
+    <div className="w-full min-h-screen bg-gray-800 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white text-center">Novo vendedor</h1>
+
+        <form className="flex flex-col my-6" onSubmit={handleSubmit}>
+          <label className="font-medium text-white">Nome do vendedor:</label>
+          <input
+            type="text"
+            placeholder="Digite o nome do vendedor..."
+            className="w-full mb-5 p-2 rounded bg-white"
+            ref={nomeRef} />
+
+          <label className="font-medium text-white">Código do vendedor:</label>
+          <input
+            type="text"
+            placeholder="Digite o código do vendedor..."
+            className="w-full mb-5 p-2 rounded bg-white"
+            ref={codRef} />
+          <label className="font-medium text-white">PIN:</label>
+          <input
+            type="text"
+            placeholder="Crie um PIN..."
+            className="w-full mb-5 p-2 rounded bg-white"
+            ref={pinRef} />
+
+
+          <input type="submit" value="Criar"
+            className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium" />
+          {erro && (
+            <>
+              <span className="text-center text-red-500 text-sm font-light mt-4">{erro}</span>
+            </>
+          )}
+        </form>
+
+        <section className="flex flex-col gap-4">
+
+          {vendedores.map((vendedor) => (
+            <CardVendedor
+              vendedor={vendedor} />
+          ))}
+
+        </section>
+      </main>
+    </div>
+  )
+}
