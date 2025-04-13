7974058a1694ec7c390c150c67d282c74e4c04b8
Jean-Luk <jeannfantozzi@gmail.com>
2025-04-13 19:31:27 -0300

    feat: adicionar rotas das páginas no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header.jsx";
 import Navbar from "./components/navbar.jsx";
 import Footer from "./components/footer.jsx";
+
 import Pedacos from "./pages/Pedacos.jsx";
+import Buscar from "./pages/Buscar.jsx";
+import Tipos from "./pages/Tipos.jsx";
+import Vendedores from "./pages/Vendedores.jsx";
 import VisualizarPedaco from "./pages/VisualizarPedaco.jsx";
-import { StrictMode } from 'react'
+import RetirarPedaco from "./pages/retirarPedaco.jsx";
+
 
 export default function App () {
 
@@ -14,9 +19,12 @@ export default function App () {
       <Header />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Pedacos />} />
+        <Route path="/" element={<Buscar />} />
         <Route path="/pedacos" element={<Pedacos />} />
         <Route path="/visualizarPedaco/:id" element={<VisualizarPedaco />} />
+        <Route path="/retirarPedaco/:id" element={<RetirarPedaco />} />
+        <Route path="/tipos" element={<Tipos />} />
+        <Route path="/vendedores" element={<Vendedores />} />
       </Routes>
       <Footer />
     </BrowserRouter>
