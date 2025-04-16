c214166a9c25546b1a77bc14fe80f8005707eb7b
Jean-Luk <jeannfantozzi@gmail.com>
2025-04-15 22:02:21 -0300

    fix: corrigir type error

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const port = import.meta.env.VITE_PORT || 8080;
-const host = import.meta.env.VITE_HOST || "localahost";
+const host = import.meta.env.VITE_HOST || "localhost";
 
 export const api = axios.create({
   baseURL: `http://${host}:${port}`
